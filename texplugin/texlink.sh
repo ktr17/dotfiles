f411c80078e7c57e8be9cb606dccfdbf6b3c2296
ktr17 <ktr199517@gmail.com>
2018-01-21 04:38:37 +0900

    texディレクトリの移動

--- texplugin/texlink.sh
+++
@@ -1,9 +0,0 @@
-ln -s ~/dotfiles/texplugin/xcolor/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/otf/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/etoolbox/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/fancyhdr/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/plistings/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/titlesec/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/tools/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/texplugin/listings/ /usr/local/texlive/texmf-local/tex/latex/local/
-mktexlsr
