01b0c0df1af22ba1ca48eb22dbdc2a14e43e46bb
ktr17 <ktr199517@gmail.com>
2018-01-21 04:38:18 +0900

    自作のtexクラスを追加

---
+++ tex/texlink.sh
@@ -0,0 +1,9 @@
+ln -s ~/dotfiles/tex/texplugin/xcolor/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/otf/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/etoolbox/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/fancyhdr/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/plistings/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/titlesec/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/tools/ /usr/local/texlive/texmf-local/tex/latex/local/
+ln -s ~/dotfiles/tex/texplugin/listings/ /usr/local/texlive/texmf-local/tex/latex/local/
+mktexlsr
