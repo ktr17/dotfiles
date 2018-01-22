0e5a6268f8b256b2f1e9819d0bb8f36d07622ccc
ktr17 <ktr199517@gmail.com>
2018-01-23 03:53:45 +0900

    texライブラリのリンクを貼るスクリプトの修正

--- tex/texlink.sh
+++ tex/texlink.sh
@@ -1,9 +1,31 @@
-ln -s ~/dotfiles/tex/texplugin/xcolor/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/otf/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/etoolbox/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/fancyhdr/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/plistings/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/titlesec/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/tools/ /usr/local/texlive/texmf-local/tex/latex/local/
-ln -s ~/dotfiles/tex/texplugin/listings/ /usr/local/texlive/texmf-local/tex/latex/local/
+#!/bin/bash
+
+ostype=$(uname -a)
+
+if [[ $ostype =~ "Darwin" ]]
+then
+    # Macの場合
+    ln -s ~/dotfiles/tex/texplugin/xcolor/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/otf/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/etoolbox/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/fancyhdr/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/plistings/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/titlesec/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/tools/ /usr/local/texlive/texmf-local/tex/latex/local/
+    ln -s ~/dotfiles/tex/texplugin/listings/ /usr/local/texlive/texmf-local/tex/latex/local/
+elif [[ $ostype =~ "Linux" ]]
+then
+    # Linuxの場合
+    ln -s ~/dotfiles/tex/texplugin/xcolor/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/otf/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/etoolbox/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/fancyhdr/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/plistings/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/titlesec/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/tools/ /usr/share/texlive/texmf-dist/tex/latex/
+    ln -s ~/dotfiles/tex/texplugin/listings/ /usr/share/texlive/texmf-dist/tex/latex/
+else
+    echo "osが不明です"
+fi
+
 mktexlsr
